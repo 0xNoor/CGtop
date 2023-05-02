efdf781552a4ce98d8f9916ff6f6ce5283a6ce99
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-02 18:45:20 +0530

    Added function for getting usage of multiple cores in PC

--- cpu-usage.py
+++ cpu-usage.py
@@ -1,11 +1,19 @@
 import time
+import os
 try:
 	import psutil;
 except:
 	print("psutil not installed");
 
+num_cores = psutil.cpu_count(logical=True);
 
 while True:
     cpu_percent = psutil.cpu_percent();
-    print(f"CPU usage: {cpu_percent}%", end="\r");
+    per_cpu_percent = psutil.cpu_percent(percpu=True);
+    for i in range(num_cores):
+         print(f"Core ID {i+1} usage is {per_cpu_percent[i]}");
+    print(f"CPU usage: {cpu_percent}%");
     time.sleep(1);
+    print("\r")
+#    os.system('clear');
+    print(f"\033[F" * (num_cores+2), end="")
