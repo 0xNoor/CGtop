3158ec2c2408ea510f9425d3bc1a5b63bdac5e07
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-06 15:17:06 +0530

    Minimal Integration of radeon_gpu into main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from nvidia_gpu import *
 from cpu_usage import *
+from radeon_gpu import *
 
 BANNER =(
 " ██████╗ ██████╗████████╗ ██████╗ ██████╗ ",
@@ -18,23 +19,32 @@ if "nvidia" in gpu_check:
 	nvmlInit();
 	for i in range(total_nvidia_gpu()):
 		GPU_ID = nvmlDeviceGetHandleByIndex(i)
+elif "amdgpu" in gpu_check:
+	pass
 else:
 	pass
 
 
 while(1):
 	os.system("clear");
-
 	for i in BANNER:
 		print(i);
+	cpu_usage();
+	per_cpu_usage();
+	per_cpu_freq();
+	#time.sleep(1);
+
+
 	if "nvidia" in gpu_check:
 		print(nvidia_gpu_name(0))
 		print(nvidia_gpu_temp(GPU_ID))
 		print(nvidia_gpu_core_clock(GPU_ID))
 		print(nvidia_gpu_mem_clock(GPU_ID))
+	elif "amdgpu" in gpu_check:
+		radeon_out = subprocess.check_output("radeontop -l 1 -d -", shell=True)
+		output = radeon_out.decode()
+		print(radeon_gpu_usage(output))
+		time.sleep(1)
 	else:
 		pass
-	cpu_usage();
-	per_cpu_usage();
-	per_cpu_freq();
-	time.sleep(1);
+
--- radeon_gpu.py
+++ radeon_gpu.py
@@ -3,14 +3,17 @@ import os
 import subprocess
 import re
 
-radeon_out = subprocess.check_output("radeontop -l 1 -d -", shell=True)
-output = radeon_out.decode()
-gpu_usage = re.search(r"gpu.+?([0-9.]+)", output).group(1)
 
-vram = re.search(r"vram.+?([0-9.]+)", output).group(1)
-vram_mem = re.search(r'vram \d+\.\d+% (\d+\.\d+)mb', output)
-vram_mb = vram_mem.group(1)
-print(f"GPU: {gpu_usage}, vram: {vram}, vram-mem {vram_mb}")
+def radeon_gpu_usage(output):
+    gpu_usage = re.search(r"gpu.+?([0-9.]+)", output).group(1)
+    return gpu_usage
+
+def radeon_gpu_vram_usage(output):
+    vram = re.search(r"vram.+?([0-9.]+)", output).group(1)
+
+def radeon_gpu_vram_mem(output):
+    vram_mem = re.search(r'vram \d+\.\d+% (\d+\.\d+)mb', output).group(1)
+
 
 
 '''
