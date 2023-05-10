c1e85bf57616f24bbff75a32b8e1aa0ccb85c616
Mohd Noor Aman <nooraman5718@gmail.com>
2023-05-10 19:23:40 +0530

    Added some more function for radeon2

--- radeon_gpu2.py
+++ radeon_gpu2.py
@@ -6,9 +6,15 @@ except:
 def total_amd_gpu(ID):
     return get_gpu(ID)
 
-def amd(ID):
-    sclk = total_amd_gpu(ID)
-    s = sclk.query_sclk()
-    return s
+def amd_gpu_core_clock(ID):
+    sclk = total_amd_gpu(ID).query_sclk()
+    return sclk
 
-print(amd(0))
+def amd_gpu_mem_clock(ID):
+    mclk =total_amd_gpu(ID).query_mclk()
+    return mclk
+def amd_gpu_vram_usage(ID):
+    vram_usage = total_amd_gpu(ID).query_vram_usage()
+    return vram_usage
+
+print(amd_gpu_vram_usage(0))
